dbd8f9f9f5ccf9dc08dedd52ebdaebe148e8c763
EXPDOM\areyes <areyes@expertime.com>
2023-12-06 11:46:16 +0100

    view for DetailedAccount

---
+++ dbo/Views/DetailedAccount.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[DetailedAccount]
+AS 
+SELECT A.[Id],
+       A.[CostumerId],
+	   C.[Name] CostumerName,
+	   C.[Surname] CostumerSurname,
+       A.[Name],
+       A.[Balance]
+FROM 
+    [dbo].[Account] A
+INNER JOIN [dbo].[Costumer] C ON A.CostumerId = C.Id
