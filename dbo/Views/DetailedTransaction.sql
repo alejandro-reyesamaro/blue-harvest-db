c5af8f552be1b5cc018d612596f040e1e910e3ce
EXPDOM\areyes <areyes@expertime.com>
2023-12-06 12:03:57 +0100

    fix view for DetailedTransaction

--- dbo/Views/DetailedTransaction.sql
+++ dbo/Views/DetailedTransaction.sql
@@ -3,13 +3,14 @@ AS
 SELECT        
     T.Id, 
     T.CostumerId, 
-    T.CostumerAccountId, 
-    T.TargetAccountId, 
-    T.Amount, 
-    T.Date, 
-    SA.Name CostumerAccountName, 
+    T.CostumerAccountId,
+    SA.Name CostumerAccountName,
+    T.TargetAccountId,
     TA.Name TargetAccountName, 
-    C.Name TargetAccountCostumerName
+    C.Name TargetAccountCostumerName,
+    C.Surname TargetAccountCostumerSurname,
+    T.Amount, 
+    T.Date
 FROM            
     [dbo].[Transaction] T 
 INNER JOIN [dbo].[Account] SA ON T.CostumerAccountId = SA.Id
